ab45123cd5c0a702a9bb950f9bf1a2e340d6fd33
dohliam <dohliam@gmail.com>
2017-02-25 19:39:56 -0800

    add gh-pages

---
+++ objects.js
@@ -0,0 +1,577 @@
+var inputstream = [{
+    "name": "anchor",
+    "tags": "watercraft, sailing"
+  }, {
+    "name": "aperture",
+    "tags": "camera, photography, circle"
+  }, {
+    "name": "arrow-down",
+    "tags": "directions, arrows, download"
+  }, {
+    "name": "arrow-up",
+    "tags": "directions, arrows, upload"
+  }, {
+    "name": "art",
+    "tags": "painting"
+  }, {
+    "name": "bar-chart",
+    "tags": "charts, graphs, data, statistics"
+  }, {
+    "name": "batteryfull",
+    "tags": "batteries, power, energy, devices"
+  }, {
+    "name": "batterylow",
+    "tags": "batteries, power, energy, devices"
+  }, {
+    "name": "bike",
+    "tags": "bicycle, biking, sports, outdoors, transportation, vehicles"
+  }, {
+    "name": "biker",
+    "tags": "bicycle, biking, sports, outdoors, transportation, vehicles"
+  }, {
+    "name": "bikewheel",
+    "tags": "bicycle, wheels, circles, transportation"
+  }, {
+    "name": "blimp",
+    "tags": "transportation, vehicle, flying"
+  }, {
+    "name": "bolt",
+    "tags": "lightning, power, energy, flash"
+  }, {
+    "name": "bomb",
+    "tags": "threat, danger, security, crash, quit"
+  }, {
+    "name": "booklet",
+    "tags": "reading, books, bookmark"
+  }, {
+    "name": "bookshelf",
+    "tags": "reading, books, library"
+  }, {
+    "name": "briefcase",
+    "tags": "travel, business, carrying"
+  }, {
+    "name": "brightness",
+    "tags": "internet, software, programs"
+  }, {
+    "name": "browser",
+    "tags": "lightbulb, idea, power"
+  }, {
+    "name": "brush-pencil",
+    "tags": "art, drawing, painting"
+  }, {
+    "name": "calculator",
+    "tags": "math, arithmetic"
+  }, {
+    "name": "calendar",
+    "tags": "dates"
+  }, {
+    "name": "camera",
+    "tags": "photography, devices"
+  }, {
+    "name": "car",
+    "tags": "automobile, transportation, vehicles"
+  }, {
+    "name": "cart",
+    "tags": "basket, shopping, carrying"
+  }, {
+    "name": "carwheel",
+    "tags": "automobile, wheels, circles, transportation"
+  }, {
+    "name": "caution",
+    "tags": "warning, attention, signs"
+  }, {
+    "name": "chat",
+    "tags": "speech, bubbles, talking"
+  }, {
+    "name": "check",
+    "tags": "checkmark, checkbox, tasklist"
+  }, {
+    "name": "circlecompass",
+    "tags": "math, geometry"
+  }, {
+    "name": "clapboard",
+    "tags": "movies, films, cinema, video"
+  }, {
+    "name": "clipboard",
+    "tags": "copy, paste"
+  }, {
+    "name": "clock",
+    "tags": "time, stopwatch, timer"
+  }, {
+    "name": "cloud",
+    "tags": "weather, cloudy"
+  }, {
+    "name": "cmyk",
+    "tags": "colors, art, graphics"
+  }, {
+    "name": "colorwheel",
+    "tags": "colors, art, graphics"
+  }, {
+    "name": "compass",
+    "tags": "navigation, directions"
+  }, {
+    "name": "compose",
+    "tags": "writing, document, editing"
+  }, {
+    "name": "computer",
+    "tags": "monitor, devices"
+  }, {
+    "name": "cone",
+    "tags": "traffic, construction"
+  }, {
+    "name": "contacts",
+    "tags": "addresses, books"
+  }, {
+    "name": "contrast",
+    "tags": "graphics, image editing"
+  }, {
+    "name": "countdown",
+    "tags": "timer, clock"
+  }, {
+    "name": "creditcard",
+    "tags": "purchasing, finance, shopping"
+  }, {
+    "name": "crop",
+    "tags": "graphics, image editing"
+  }, {
+    "name": "crossroads",
+    "tags": "directions, navigation, signs"
+  }, {
+    "name": "cruise",
+    "tags": "ship, travel, transportation, vehicles, watercraft"
+  }, {
+    "name": "cursor",
+    "tags": "arrows, pointer, interface, click"
+  }, {
+    "name": "denied",
+    "tags": "forbidden, restricted"
+  }, {
+    "name": "dev",
+    "tags": "coding, programming, html"
+  }, {
+    "name": "die",
+    "tags": "dice, games, chance, random"
+  }, {
+    "name": "document",
+    "tags": "paper, file, editing"
+  }, {
+    "name": "dolly",
+    "tags": "transportation, vehicles"
+  }, {
+    "name": "door",
+    "tags": "entrance, entry, enter, home, homepage, access"
+  }, {
+    "name": "download",
+    "tags": "arrows, cloud"
+  }, {
+    "name": "easel",
+    "tags": "art, painting, charts"
+  }, {
+    "name": "email",
+    "tags": "at, symbols"
+  }, {
+    "name": "eye",
+    "tags": "eyeball"
+  }, {
+    "name": "eyedropper",
+    "tags": "colors, graphics"
+  }, {
+    "name": "fashion",
+    "tags": "shoe, heel"
+  }, {
+    "name": "filmreel",
+    "tags": "films, movies, cinema"
+  }, {
+    "name": "filmroll",
+    "tags": "photography, camera"
+  }, {
+    "name": "flag",
+    "tags": "flags, markers, completed"
+  }, {
+    "name": "flame",
+    "tags": "fire, elements"
+  }, {
+    "name": "flash",
+    "tags": "storage, devices, memory, save"
+  }, {
+    "name": "flower",
+    "tags": "nature, photography, picture, outdoors"
+  }, {
+    "name": "focus",
+    "tags": "camera, photography, pictures, images"
+  }, {
+    "name": "folder",
+    "tags": "directory"
+  }, {
+    "name": "frames",
+    "tags": "pictures, photography, slides"
+  }, {
+    "name": "gamecontroller",
+    "tags": "games, gaming, videogames"
+  }, {
+    "name": "gas",
+    "tags": "vehicles, cars, automobiles"
+  }, {
+    "name": "gear",
+    "tags": "settings, preferences, options, circles"
+  }, {
+    "name": "genius",
+    "tags": "atom, nuclear"
+  }, {
+    "name": "global",
+    "tags": "earth, world, map, circles"
+  }, {
+    "name": "globe",
+    "tags": "earth, world, map"
+  }, {
+    "name": "gps",
+    "tags": "navigation, arrows, directions, compass, north"
+  }, {
+    "name": "hazard",
+    "tags": "warning, fallout, radiation, signs"
+  }, {
+    "name": "heart",
+    "tags": "love, favorite"
+  }, {
+    "name": "helicopter",
+    "tags": "transportation, vehicles, flying"
+  }, {
+    "name": "hotair",
+    "tags": "balloon, travel, transportation, vehicles, flying"
+  }, {
+    "name": "hourglass",
+    "tags": "timer, timing, time, waiting, loading, countdown"
+  }, {
+    "name": "image",
+    "tags": "graphics, photos, slides, photography"
+  }, {
+    "name": "interstate",
+    "tags": "transportation, highway, signs"
+  }, {
+    "name": "key",
+    "tags": "access"
+  }, {
+    "name": "keyboard",
+    "tags": "peripherals, devices, computer"
+  }, {
+    "name": "lens",
+    "tags": "campera, photography"
+  }, {
+    "name": "lightbulb",
+    "tags": "idea, household, home, appliances"
+  }, {
+    "name": "loading",
+    "tags": "progress, brightness, light, pulse"
+  }, {
+    "name": "location",
+    "tags": "navigation, directions, compass, gps, maps"
+  }, {
+    "name": "locked",
+    "tags": "forbidden, restricted, secure"
+  }, {
+    "name": "magicwand",
+    "tags": "magic, special"
+  }, {
+    "name": "magnifyingglass",
+    "tags": "search, expand, zoom, enlarge, find"
+  }, {
+    "name": "mail",
+    "tags": "envelope, post"
+  }, {
+    "name": "map",
+    "tags": "navigation, gps, directions"
+  }, {
+    "name": "megaphone",
+    "tags": "sound, speaker, volume"
+  }, {
+    "name": "megaphone2",
+    "tags": "sound, speaker, volume"
+  }, {
+    "name": "memorycard",
+    "tags": "storage"
+  }, {
+    "name": "merge",
+    "tags": "arrows, directions, signs"
+  }, {
+    "name": "mic",
+    "tags": "recording, audio, sound"
+  }, {
+    "name": "microphone",
+    "tags": "recording, audio, sound"
+  }, {
+    "name": "money",
+    "tags": "purchasing, finance, currency, shopping"
+  }, {
+    "name": "motorcycle",
+    "tags": "transportation, vehicles"
+  }, {
+    "name": "music",
+    "tags": "audio, sound, musical, note"
+  }, {
+    "name": "news",
+    "tags": "newspaper, updates, information"
+  }, {
+    "name": "paintbrush",
+    "tags": "painting, art"
+  }, {
+    "name": "paintbrush2",
+    "tags": "painting, art"
+  }, {
+    "name": "paintcan",
+    "tags": "painting, fill"
+  }, {
+    "name": "paintroller",
+    "tags": "painting"
+  }, {
+    "name": "parachute",
+    "tags": "recreation, escape, leave, quit"
+  }, {
+    "name": "pencil",
+    "tags": "editing, writing, stationery"
+  }, {
+    "name": "phone",
+    "tags": "telecommunications, telephone"
+  }, {
+    "name": "piechart",
+    "tags": "charts, graphs"
+  }, {
+    "name": "pin",
+    "tags": "marker"
+  }, {
+    "name": "pin2",
+    "tags": "marker, bookmark, navigation"
+  }, {
+    "name": "plane",
+    "tags": "transportation, vehicles, flying, airplane"
+  }, {
+    "name": "play",
+    "tags": "audio, video"
+  }, {
+    "name": "plugin",
+    "tags": "power, charge, energy"
+  }, {
+    "name": "polaroid",
+    "tags": "photography, photos, pictures"
+  }, {
+    "name": "polaroidcamera",
+    "tags": "photography, cameras"
+  }, {
+    "name": "polaroids",
+    "tags": "photography, photos, pictures, album"
+  }, {
+    "name": "power",
+    "tags": "energy, shutdown"
+  }, {
+    "name": "present",
+    "tags": "gift, wrapping, bow, box"
+  }, {
+    "name": "profle",
+    "tags": "head, silhouette, personal"
+  }, {
+    "name": "quote",
+    "tags": "typography"
+  }, {
+    "name": "racingflags",
+    "tags": "flags, sports, competition"
+  }, {
+    "name": "radio",
+    "tags": "broadcasting"
+  }, {
+    "name": "radiotower",
+    "tags": "broadcasting, radio, wireless, telecommunications"
+  }, {
+    "name": "rainbow",
+    "tags": "colors"
+  }, {
+    "name": "recycle",
+    "tags": "recycling, reuse, reduce"
+  }, {
+    "name": "rgb",
+    "tags": "colors, graphics"
+  }, {
+    "name": "ribbon",
+    "tags": "markers, reward, prize, award"
+  }, {
+    "name": "roadblock",
+    "tags": "transportation, traffic"
+  }, {
+    "name": "rocket",
+    "tags": "transportation, space, rocketship, vehicles"
+  }, {
+    "name": "rulertriangle",
+    "tags": "math, geometry, measurement, tools"
+  }, {
+    "name": "running",
+    "tags": "exercise, sports, jogging, jogger"
+  }, {
+    "name": "sailboat",
+    "tags": "sailing, ship, boat, transportation, vehicles, watercraft"
+  }, {
+    "name": "schooolbus",
+    "tags": "vehicles, transportation, automobile"
+  }, {
+    "name": "scissors",
+    "tags": "tools, crafts, cut, clip"
+  }, {
+    "name": "scooter",
+    "tags": "vehicles, transportation, motorbike"
+  }, {
+    "name": "security",
+    "tags": "shield, secure, safety"
+  }, {
+    "name": "selftimer",
+    "tags": "clock, time, timer, stopwatch"
+  }, {
+    "name": "settings",
+    "tags": "preferences, options"
+  }, {
+    "name": "shipwheel",
+    "tags": "sailing, watercraft, wheels"
+  }, {
+    "name": "shoeprints",
+    "tags": "feet, tracks, navigation"
+  }, {
+    "name": "shop",
+    "tags": "ticket, price, shopping, purchase"
+  }, {
+    "name": "skateboard",
+    "tags": "vehicles, transportation, recreation, sports"
+  }, {
+    "name": "slr",
+    "tags": "camera, photography, devices"
+  }, {
+    "name": "smartphone",
+    "tags": "mobile, telephone, cellphone, devices"
+  }, {
+    "name": "spaceshuttle",
+    "tags": "transportation, vehicles, rocketship, space"
+  }, {
+    "name": "speaker",
+    "tags": "volume, audio, sound"
+  }, {
+    "name": "speedometer",
+    "tags": "transportation, speed, dashboard, meter"
+  }, {
+    "name": "spraypaint",
+    "tags": "painting, art, graphics"
+  }, {
+    "name": "stack",
+    "tags": "graphics, layers"
+  }, {
+    "name": "star",
+    "tags": "award, reward, prize, rate, rating, space"
+  }, {
+    "name": "steeringwheel",
+    "tags": "transportation, wheels, automobile, car"
+  }, {
+    "name": "stop",
+    "tags": "forbidden, restricted, access, security"
+  }, {
+    "name": "sub",
+    "tags": "audio, sound, music, speakers, subwoofer"
+  }, {
+    "name": "submarine",
+    "tags": "transportation, vehicles, watercraft, boat, ship"
+  }, {
+    "name": "support",
+    "tags": "help, assistance, sailing, safety"
+  }, {
+    "name": "swatches",
+    "tags": "colors, graphics"
+  }, {
+    "name": "tablet",
+    "tags": "devices, mobile"
+  }, {
+    "name": "takeoff",
+    "tags": "transportation, airport, airplane, flight, flying, aviation"
+  }, {
+    "name": "target",
+    "tags": "goal, objective, bullseye"
+  }, {
+    "name": "taxi",
+    "tags": "transportation, vehicles, automobiles, traffic"
+  }, {
+    "name": "toolbox",
+    "tags": "tools, toolkit"
+  }, {
+    "name": "tools",
+    "tags": "wrench, screwdriver, options, settings, preferences"
+  }, {
+    "name": "tractor",
+    "tags": "transportation, vehicles, farming"
+  }, {
+    "name": "traffic",
+    "tags": "trafficlight, colors, transportation"
+  }, {
+    "name": "train",
+    "tags": "transportation, vehicles, subway, metro, underground, rail"
+  }, {
+    "name": "travelerbag",
+    "tags": "travel, journey, wander"
+  }, {
+    "name": "trends",
+    "tags": "graphs, charts, data, statistics"
+  }, {
+    "name": "tripod",
+    "tags": "photography, camera, equipment"
+  }, {
+    "name": "trophy",
+    "tags": "prize, award, reward, tournament"
+  }, {
+    "name": "truck",
+    "tags": "transportation, vehicles, automobiles, traffic"
+  }, {
+    "name": "tv",
+    "tags": "television, telecommunications, broadcast, video"
+  }, {
+    "name": "typography",
+    "tags": "letters, alphabet, writing, text"
+  }, {
+    "name": "ufo",
+    "tags": "transportation, vehicles, space, flight, spaceship, aliens"
+  }, {
+    "name": "umbrella",
+    "tags": "weather, rain"
+  }, {
+    "name": "unicycle",
+    "tags": "cycling, sports, exercise, transportation, vehicles"
+  }, {
+    "name": "unlocked",
+    "tags": "security, locks, open"
+  }, {
+    "name": "upload",
+    "tags": "arrows, cloud"
+  }, {
+    "name": "video",
+    "tags": "film, movies, cinema"
+  }, {
+    "name": "videocameraclassic",
+    "tags": "film, movies, cinema, video"
+  }, {
+    "name": "videocameracompact",
+    "tags": "film, movies, cinema, video, devices, recording, camcorder"
+  }, {
+    "name": "volume",
+    "tags": "audio, sound, levels, graphs, charts"
+  }, {
+    "name": "water",
+    "tags": "elements, drop, dropper, graphics"
+  }, {
+    "name": "weather",
+    "tags": "cloud, cloudy, sun"
+  }, {
+    "name": "windsock",
+    "tags": "weather, windy"
+  }, {
+    "name": "windy",
+    "tags": "weather, arrows, directions"
+  }, {
+    "name": "x",
+    "tags": "checkbox, checkmark, cross, tasklist"
+  }, {
+    "name": "zoomin",
+    "tags": "magnify, increase, expand, resize"
+  }, {
+    "name": "zoomout",
+    "tags": "reduce, decrease, compress, resize"
+}];
